91e61b4991ad0221049365a90c7113fa28fb8af6
Biralo <biralonet@gmail.com>
2026-02-14 22:27:51 -0800

    Fix: Prevent modified indicator from appearing when opening a file

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,11 @@
-// Unified CodeMirror imports to prevent multiple @codemirror/state instances
-import { EditorView, basicSetup } from "https://esm.sh/codemirror@6.0.1?deps=@codemirror/state@6.4.1";
-import { EditorState } from "https://esm.sh/@codemirror/state@6.4.1";
-import { keymap } from "https://esm.sh/@codemirror/view@6.34.1?deps=@codemirror/state@6.4.1";
-import { indentWithTab } from "https://esm.sh/@codemirror/commands@6.7.1?deps=@codemirror/state@6.4.1";
-import { StreamLanguage } from "https://esm.sh/@codemirror/language@6.10.3?deps=@codemirror/state@6.4.1";
+import { 
+    EditorView, 
+    basicSetup, 
+    keymap, 
+    EditorState, 
+    StreamLanguage, 
+    indentWithTab 
+} from "https://esm.sh/codemirror@6.0.1?deps=@codemirror/state@6.4.1";
 
 const verilogKeywords = "always|and|assign|automatic|begin|buf|bufif0|bufif1|case|casex|casez|cell|config|deassign|default|defparam|design|disable|edge|else|end|endcase|endconfig|endfunction|endgenerate|endmodule|endprimitive|endspecify|endtable|endtask|event|for|force|forever|fork|function|generate|genvar|highz0|highz1|if|ifnone|incdir|include|initial|inout|input|instance|integer|join|large|liblist|library|localparam|macromodule|medium|module|nand|negedge|nmos|nor|noshowcancelled|not|notif0|notif1|or|output|parameter|pmos|posedge|primitive|pull0|pull1|pulldown|pullup|pulsestyle_onevent|pulsestyle_ondetect|rcmos|real|realtime|reg|release|repeat|rnmos|rpmos|rtran|rtranif0|rtranif1|scalared|showcancelled|signed|small|specify|specparam|strong0|strong1|supply0|supply1|table|task|time|tran|tranif0|tranif1|tri|tri0|tri1|triand|trior|trireg|unsigned|use|vectored|wait|wand|weak0|weak1|while|wire|wor|xnor|xor";
 
@@ -28,6 +30,7 @@ const verilogLanguage = StreamLanguage.define({
 export class Editor {
     constructor(domElement, onDocChange) {
         this.onDocChange = onDocChange;
+        this.ignoreChanges = false;
         try {
             this.view = new EditorView({
                 state: EditorState.create({
@@ -36,7 +39,7 @@ export class Editor {
                         keymap.of([indentWithTab]),
                         verilogLanguage,
                         EditorView.updateListener.of((update) => {
-                            if (update.docChanged) {
+                            if (update.docChanged && !this.ignoreChanges) {
                                 this.onDocChange();
                             }
                         })
@@ -51,9 +54,11 @@ export class Editor {
     }
 
     setContent(content) {
+        this.ignoreChanges = true;
         this.view.dispatch({
             changes: { from: 0, to: this.view.state.doc.length, insert: content }
         });
+        this.ignoreChanges = false;
     }
 
     getContent() {
